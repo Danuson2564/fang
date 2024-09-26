dd1c92a9adf65e32c907de64fc3b18ef93f6c20d
y <dnusrnsaenprasiththi@gmail.com>
2024-09-26 20:46:39 +0700

    docs

---
+++ docs/index.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+// ฟังก์ชันสำหรับแสดงหน้าและรวมส่วนประกอบ
+function load_view($view) {
+    include 'views/header.php';  // รวมส่วน header
+    include 'views/navbar.php';  // รวมส่วน navbar
+    include 'views/sitebar.php'; // รวมส่วน sidebar (แก้ไขเป็น sidebar ถ้าเขียนผิด)
+    include 'views/' . $view . '.php'; // รวมเนื้อหาของหน้า
+    include 'views/footer.php';  // รวมส่วน footer
+}
+
+// รับ path จาก URL
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Routing แบบง่าย
+switch ($path) {
+    case '/':
+        load_view('home'); // รวมไฟล์ home.php ที่อยู่ในโฟลเดอร์ views
+        break;
+    // case '/about':
+    //     load_view('about');
+    //     break;
+    // case '/contact':
+    //     load_view('contact');
+    //     break;
+    default:
+        http_response_code(404);
+        echo "404 Page Not Found";
+        break;
+}
---
+++ docs/views/navbar.php
@@ -0,0 +1,14 @@
+﻿<header class="topbar position-relative">
+            <div class="navbar navbar-header bg-white d-flex align-items-center justify-content-between p-3 fixed-top">
+               <a href="../index.html">
+                  <b class="logo-icon d-xl-none d-block">
+                     <img src="../assets/images/logos/dark-logo.svg" alt="homepage" class="dark-logo">
+                  </b>
+               </a>
+               <div class="d-flex align-items-center">
+                  <a class="fs-8 text-muted d-xl-none d-block sidebartoggler" href="javascript:void(0)" role="button">
+                     <i class="ti ti-menu-2"></i>
+                  </a>
+               </div>
+            </div>
+         </header>
